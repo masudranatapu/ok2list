e9866f5cd814ae44be17de30cc993d82dbcdf1b5
Masud Rana Tapu <91066147+masudranatapu@users.noreply.github.com>
2023-01-16 18:23:08 +0600

    done

--- resources/views/admin/currency/index.blade.php
+++ resources/views/admin/currency/index.blade.php
@@ -63,77 +63,65 @@
                     </div>
                     <div class="card-body">
                         <table class="table table-hover text-nowrap table-bordered">
-                            <thead>
-                                <tr>
-                                    <th class="text-center">#</th>
-                                    <th class="text-center">Name</th>
-                                    <th class="text-center">Code</th>
-                                    <th class="text-center">Direction</th>
-                                    <th class="text-center">Flag</th>
-                                    <th class="text-center">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <table class="table table-hover text-nowrap table-bordered">
-                                    <thead class="text-center">
+                            <table class="table table-hover text-nowrap table-bordered">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th width="5%">#</th>
+                                        <th>Name</th>
+                                        <th>Code</th>
+                                        <th>Symbol</th>
+                                        <th>Position</th>
+                                        <th width="15%">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    @forelse ($currencies as $key => $currency)
                                         <tr>
-                                            <th width="5%">#</th>
-                                            <th>Name</th>
-                                            <th>Code</th>
-                                            <th>Symbol</th>
-                                            <th>Position</th>
-                                            <th width="15%">Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="text-center">
-                                        @forelse ($currencies as $key => $currency)
-                                            <tr>
-                                                <td>{{ $key + 1 }}</td>
-                                                <td>
-                                                    {{ $currency->name }}
-                                                    @if ($currency->default_currencies == 1)
-                                                        <span class="badge badge-pill badge-primary">
-                                                            {{ __('Default') }}
-                                                        </span>
-                                                    @endif
-                                                </td>
-                                                <td>{{ $currency->code }}</td>
-                                                <td>{{ $currency->symbol }}</td>
-                                                <td>{{ Str::ucfirst($currency->symbol_position) }}</td>
-                                                <td class="d-flex justify-content-center align-items-center">
-                                                    @if ($currency->code == 'USD')
-                                                        <a href="javascript:void(0)" class="btn btn-warning btn-sm mt-0 mr-2" data-toggle="tooltip" title="{{ __('you_can_not_delete_or_edit_this_currency') }}">
-                                                            <i class="la la-cog"></i>
-                                                        </a>
-                                                    @endif
-                                                    @if ($currency->code != 'USD')
-                                                        <a href="{{ route('currency.edit', $currency->id) }}" class="btn btn-info btn-sm mt-0 mr-2">
-                                                            <i class="la la-edit"></i>
-                                                        </a>
-                                                        <form action="{{ route('currency.destroy', $currency->id) }}" class="d-inline" method="POST">
-                                                            @method('DELETE')
-                                                            @csrf
-                                                            <button data-toggle="tooltip" data-placement="top" title="{{ __('Delete Currency') }}" onclick="return confirm('{{ __('are_you_sure_want_to_delete_this_item') }}');" class="btn btn-sm bg-danger">
-                                                                <i class="la la-trash"></i>
-                                                            </button>
-                                                        </form>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @empty
-                                            <tr>
-                                                <td colspan="10" class="text-center">
-                                                    <h5 class="pt-3">No data</h5>
-                                                    <a class="btn btn-success mb-3" href="{{ route('currency.create') }}">
-                                                        <i class="la la-plus-circle"></i>
-                                                        Create Currency
+                                            <td>{{ $key + 1 }}</td>
+                                            <td>
+                                                {{ $currency->name }}
+                                                @if ($currency->default_currencies == 1)
+                                                    <span class="badge badge-pill badge-primary">
+                                                        {{ __('Default') }}
+                                                    </span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $currency->code }}</td>
+                                            <td>{{ $currency->symbol }}</td>
+                                            <td>{{ Str::ucfirst($currency->symbol_position) }}</td>
+                                            <td class="d-flex justify-content-center align-items-center">
+                                                @if ($currency->code == 'USD')
+                                                    <a href="javascript:void(0)" class="btn btn-warning btn-sm mt-0 mr-2" data-toggle="tooltip" title="{{ __('you_can_not_delete_or_edit_this_currency') }}">
+                                                        <i class="la la-cog"></i>
                                                     </a>
-                                                </td>
-                                            </tr>
-                                        @endforelse
-                                    </tbody>
-                                </table>
-                            </tbody>
+                                                @endif
+                                                @if ($currency->code != 'USD')
+                                                    <a href="{{ route('currency.edit', $currency->id) }}" class="btn btn-info btn-sm mt-0 mr-2">
+                                                        <i class="la la-edit"></i>
+                                                    </a>
+                                                    <form action="{{ route('currency.destroy', $currency->id) }}" class="d-inline" method="POST">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button data-toggle="tooltip" data-placement="top" title="{{ __('Delete Currency') }}" onclick="return confirm('{{ __('are_you_sure_want_to_delete_this_item') }}');" class="btn btn-sm bg-danger">
+                                                            <i class="la la-trash"></i>
+                                                        </button>
+                                                    </form>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="10" class="text-center">
+                                                <h5 class="pt-3">No data</h5>
+                                                <a class="btn btn-success mb-3" href="{{ route('currency.create') }}">
+                                                    <i class="la la-plus-circle"></i>
+                                                    Create Currency
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
                         </table>
                     </div>
                 </div>
