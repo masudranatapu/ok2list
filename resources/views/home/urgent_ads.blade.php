f3ed2b342c7946770d781643a19535634597e91b
Rony Mia <ronymia.tech@gmail.com>
2023-01-17 18:38:10 +0600

    ..

--- resources/views/home/urgent_ads.blade.php
+++ resources/views/home/urgent_ads.blade.php
@@ -55,54 +55,6 @@
                 </div>
              --}}
 
-<<<<<<< HEAD
-            <div class="featured-slider urgent premim_ads">
-              <div class="row" style="margin-right:0px !important;">
-                 @if($data['recent_ads'] && count($data['recent_ads']) > 0 )
-                        @foreach($data['recent_ads'] as $key => $ad)
-                    <div class="col-md-6 col-lg-3 mb-4">
-                        <div class="card freatured_card">
-                              <div class="featured-image">
-                                  <span class="featured-ad doorstep_tag">Doorstep</span>
-                              </div>
-                              <div class="featured-image">
-                                    <a href="{{route('ad.details',['pk_no' => $ad->pk_no, 'url_slug' => $ad->url_slug])}}"><img src="{{ asset('assets/images/default-load.png')}}"  data-src="{{ asset($ad->img_path_thumb)}}" alt="{{$ad->ad_title}}" class="img-fluid"></a>
-                                    @if('Urgent' == $ad->promotion)
-                                        <span class="featured-ad">Urgent</span>
-                                    @endif
-                                </div>
-                              <div class="card-body">
-                                    <h3 class="item-price"> {{ changeCurrency($ad->price) }} </h3>
-                                    <h4 class="item-title"><a href="{{route('ad.details',['pk_no' => $ad->pk_no, 'url_slug' => $ad->url_slug])}}">{{ $ad->ad_title }}</a></h4>
-                                    <div class="item-cat">
-                                        <span><a href="{{route('ad.details',['pk_no' => $ad->pk_no, 'url_slug' => $ad->url_slug])}}">{{ $ad->subcategory->name ?? '' }}</a></span>
-                                    </div>
-                                    @php
-                                        $payment = App\Payments::where('f_customer_pk_no',$ad->customer_pk_no)->where('status','VALID')->orderBy('pk_no','desc')->first();
-                                    @endphp
-                                    @if($payment)
-                                        <div class="premier_ads">
-                                            <span class="member">
-                                                <i class="fa fa-star"></i>
-                                                Member
-                                            </span>
-                                            <span class="verified">
-                                                <i class="fa fa-check"></i>
-                                                Verified Seller
-                                            </span>
-                                        </div>
-                                        @endif
-                                    <div class="bottom">
-                                        <p>
-                                            <a href="{{route('ad.details',['pk_no' => $ad->pk_no, 'url_slug' => $ad->url_slug])}}">{{date('d M Y h:i A',strtotime($ad->created_at))}} </a>
-                                            <span class="pull-right">
-                                                 <a href="javascript:void(0)" data-toggle="tooltip" data-placement="top" title="{{$ad->area->name }}, {{ $ad->city_division == 'city' ? $ad->area->city->name : '' }} {{ $ad->city_division == 'division' ? $ad->area->division->name : '' }}"><i class="fa fa-map-marker-alt"></i> </a>
-                                            </span>
-                                        </p> 
-                                    </div>   
-                              </div>
-                        </div>
-=======
          <div class="featured-slider urgent premim_ads">
              <div class="row" style="margin-right:0px !important;">
                  @if ($data['recent_ads'] && count($data['recent_ads']) > 0)
@@ -163,7 +115,6 @@
                                      </div>
                                  </div>
                              </div>
->>>>>>> nayeem
 
                              <!-- <div class="featured">
                             <div class="featured-image">
@@ -179,20 +130,10 @@
                                     <span><a href="{{ route('ad.details', ['pk_no' => $ad->pk_no, 'url_slug' => $ad->url_slug]) }}">{{ $ad->subcategory->name ?? '' }}</a></span>
                                 </div>
                                 @php
-<<<<<<< HEAD
                                     $payment = App\Payments::where('f_customer_pk_no',$ad->customer_pk_no)->where('status','VALID')->orderBy('pk_no','desc')->first();
                                 @endphp
                             @if($payment)
                                 <div class="premier_ads">
-=======
-                                    $payment = App\Payments::where('f_customer_pk_no', $ad->customer_pk_no)
-                                        ->where('status', 'VALID')
-                                        ->orderBy('pk_no', 'desc')
-                                        ->first();
-                                @endphp
-                            @if ($payment)
-<div class="premier_ads">
->>>>>>> nayeem
                                     <span class="member">
                                         <i class="fa fa-star"></i>
                                         Member
