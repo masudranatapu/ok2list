1812d40a56d81b88b9fbd56bc6745c1834595d95
unknown <rabinmia7578@gmail.com>
2023-02-08 15:32:29 +0600

    update

--- resources/views/ads/_topbar.blade.php
+++ resources/views/ads/_topbar.blade.php
@@ -6,10 +6,10 @@ $selected_area = $data['area_query_display'] == null ? 'srilanka' : $data['area_
         <li><a href="{{ route('home') }}">{{ __('home') }}</a></li>
         <li><a href="{{ route('ads.list') }}">{{ __('all_ads') }}</a></li>
         @if (isset($data['category_name']) && $data['category_name'] != null)
-            <li>{{ $data['category_name'] }}</li>
+        <li>{{ $data['category_name'] }}</li>
         @endif
         @if (isset($data['get_area_query']) && $data['get_area_query'] != null)
-            <li>{{ $data['get_area_query'] }}</li>
+        <li>{{ $data['get_area_query'] }}</li>
         @endif
     </ol>
 </div>
@@ -28,20 +28,31 @@ $selected_area = $data['area_query_display'] == null ? 'srilanka' : $data['area_
 @endif -->
 
 <div class="banner">
-    <div class="banner-form banner-form-full">
+    <div class="list_page banner-form banner-form-full">
         <form onsubmit="event.preventDefault()">
-            <i class="fa fa-map-marker-alt"></i>
-            <div class="dropdown category-dropdown select-category" data-toggle="modal"
-                data-target="#divisioncitymodal">
-                <span>{{ $selected_area }}</span>
+            <div class="row no-gutters">
+                <div class="col-md-6 col-lg-3">
+                    <div class="dropdown category-dropdown select-category" data-toggle="modal"
+                        data-target="#divisioncitymodal">
+                        <i class="fa fa-map-marker"></i>
+                        <span>{{ $selected_area }}</span>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+
+                    <div class="dropdown category-dropdown select-category" data-toggle="modal"
+                        data-target="#exampleModal">
+                        <i class="fa fa-tag"></i>
+                        <span>{{ $data['category_query_display'] ?? 'Select Category' }}</span>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <input type="text" class="form-control banner_search" placeholder="Type Your key word" id="keywords"
+                        value="{{ request()->get('keywords') }}">
+                    <button type="button" class="form-control" value="Search" id="key_search">{{ __('search')
+                        }}</button>
+                </div>
             </div>
-            <i class="fa fa-tag"></i>
-            <div class="dropdown category-dropdown select-category" data-toggle="modal" data-target="#exampleModal">
-                <span>{{ $data['category_query_display'] ?? 'Select Category' }}</span>
-            </div>
-            <input type="text" class="form-control banner_search" placeholder="Type Your key word" id="keywords"
-                value="{{ request()->get('keywords') }}">
-            <button type="button" class="form-control" value="Search" id="key_search">{{ __('search') }}</button>
         </form>
     </div>
 </div>
